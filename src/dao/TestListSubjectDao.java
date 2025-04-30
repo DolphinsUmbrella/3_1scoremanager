8912c0bd4aa606dbd0ac0f64bd970d5d50c2a19d
dolphin <lemonorange102@gmail.com>
2025-04-30 11:32:33 +0900

    引数等クラス図通りになるよう変更、実装完了

--- src/dao/TestListSubjectDao.java
+++ src/dao/TestListSubjectDao.java
@@ -3,92 +3,60 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import bean.Test;
+import bean.School;
+import bean.Subject;
 import bean.TestListSubject;
 
 public class TestListSubjectDao extends Dao{
 
-	public List<TestListSubject> get(String class_num) throws Exception{
+	public List<TestListSubject> postFilter(ResultSet rSet,School school) throws Exception{
 		List<TestListSubject> list = new ArrayList<>();
 
-		Connection con = getConnection();
-		//テーブル情報を全取得したい場合は引数に空文字を指定してください
-		PreparedStatement st = con.prepareStatement(
-			"select student.entYear,student.no,student.name,student.class_num,test.point from test"
-			+ "inner join student "
-			+ "on student.class_num=test.class_num"
-			+ "where class_num like ?");
-		st.setString(1, "%"+class_num+"%");
-		ResultSet rs = st.executeQuery();
-
-		while (rs.next()){
+		while(rSet.next()){
 			TestListSubject ts = new TestListSubject();
-			Test t = new Test();
-			ts.setEntYear(rs.getInt("entyear"));
-			ts.setStudentNo(rs.getString("studentno"));
-			ts.setStudentName(rs.getString("studentname"));
-			ts.setClassNum(rs.getString("classnum"));
-			t.setPoint(rs.getInt("points"));
-			list.add(ts);
-		}
-		st.close();
-		con.close();
+			ts.setEntYear(rSet.getInt("ent_year"));
+			ts.setClassNum(rSet.getString("class_num"));
+			ts.setStudentNo(rSet.getString("student_no"));
+			ts.setStudentName(rSet.getString("name"));
 
-		return list;
-	}
-	public List<TestListSubject> postFilter(ResultSet rSet,String school) throws Exception{
-		List<TestListSubject> list = new ArrayList<>();
-		try{
-			while(rSet.next()){
-				TestListSubject ts = new TestListSubject();
-				ts.setSubjectName(rSet.getString("subjectname"));
-				ts.setSubjectCd(rSet.getString("subjectcd"));
-				ts.setNum(rSet.getInt("num"));
-				ts.setPoint(rSet.getInt("point"));
-				list.add(ts);
-			}
-		}
-		catch(SQLException | NullPointerException e){
-			e.printStackTrace();
+			Map<Integer, Integer> points = new HashMap<>();
+			points.put(rSet.getInt("no"), rSet.getInt("point"));
+			ts.setPoints(points);
 		}
 
 		return list;
 	}
 
 	public List<TestListSubject> filter
-	(String subjectName, String subjectCd, int no, int point
+	(int entYear, String classNum, Subject subject, School school
 			) throws Exception{
-	List<TestListSubject> list = new ArrayList<>();
 
 	Connection con = getConnection();
 
+	//STUDENT_NO  	SUBJECT_CD  	SCHOOL_CD  	NO  	POINT  	CLASS_NUM
 	PreparedStatement st = con.prepareStatement(
-		"select subject.name,test.subjectcd,test.no,test.point from subject,test"
-		+"inner join subject "
-		+"on subject.subject_cd=test.subject_cd"
-		+"where subjectname = ? "
-		+"and subjectcd = ? "
-		+"and no = ? "
-		+"and point = ?");
-	st.setString(1, subjectName);
-	st.setString(2, subjectCd);
-	st.setInt(3, no);
-	st.setInt(4, point);
+		"select b.ent_year, b.class_num, b.no as student_no,b.name, a.subject_cd, a.no, a.point "+
+		"from test as a "+
+		"join student as b "+
+		"on a.student_no = b.no "+
+		"where b.ent_year = ? "+
+		"and b.class_num = ? "+
+		"and a.subject_cd = ? "+
+		"and a.school_cd = ?");
+	st.setInt(1, entYear);
+	st.setString(2, classNum);
+	st.setString(3, subject.getCd());
+	st.setString(4, school.getCd());
 
 	ResultSet rs = st.executeQuery();
 
-	while (rs.next()){
-		TestListSubject ts = new TestListSubject();
-		ts.setSubjectName(rs.getString("subjectname"));
-		ts.setSubjectCd(rs.getString("subjectcd"));
-		ts.setNum(rs.getInt("num"));
-		ts.setPoint(rs.getInt("point"));
-		list.add(ts);
-	}
+	List<TestListSubject> list = postFilter(rs, school);
+
 	st.close();
 	con.close();
 
