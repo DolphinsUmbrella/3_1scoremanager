26beef80a0333b905ab197c5b0c87a13a61af057
dolphin <lemonorange102@gmail.com>
2025-05-01 12:41:58 +0900

    仮実装

--- src/main/TestListSubjectExecuteAction.java
+++ src/main/TestListSubjectExecuteAction.java
@@ -1,12 +1,17 @@
 package main;
 
+import java.util.List;
 import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.Subject;
 import bean.Teacher;
+import bean.TestListSubject;
+import dao.SubjectDao;
+import dao.TestListSubjectDao;
 import tool.Action;
 
 public class TestListSubjectExecuteAction extends Action{
@@ -20,7 +25,28 @@ public class TestListSubjectExecuteAction extends Action{
 			return "null";
 		}
 
-		
+		//値の格納
+		int entYear = Integer.parseInt(request.getParameter("ent_year"));
+		String classNum = request.getParameter("class_num");
+		SubjectDao subDao = new SubjectDao();
+
+		//本来はこれが正しいです、subjectDaoが完成次第こっちに
+		//Subject subject = subDao.get(request.getParameter("subject"), user.getSchool());
+
+		//仮実装部分
+		Subject subject = new Subject();
+		subject.setCd(request.getParameter("subject"));
+		subject.setName("仮科目名");
+
+		TestListSubjectDao tsubDao = new TestListSubjectDao();
+		//入学年度、クラス番号、学生番号、氏名、点数を取得
+		List<TestListSubject> tsubList = tsubDao.filter(entYear, classNum, subject, user.getSchool());
+
+		//リクエストスコープに値を格納
+		//検索結果
+		request.setAttribute("tsubList", tsubList);
+		//科目名
+		request.setAttribute("subectName", subject.getName());
 
 		return "test_list_subject.jsp";
 	}
