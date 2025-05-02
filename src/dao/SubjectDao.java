bfe53a308a3f54b57556b5aca22a8b099a7bfe9e
o-hara_hibiki <kmm2459313@stu.o-hara.ac.jp>
2025-05-02 12:39:43 +0900

    subjectDAOをクラス図どおりに変更

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -3,9 +3,8 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
+import bean.School;
 import bean.Subject;
 
 public class SubjectDao extends Dao{
@@ -18,7 +17,7 @@ public class SubjectDao extends Dao{
 	//	get(cd:String, school:School): Subject
 	//()内は引数、後ろのSubjectは返り値です
 	//22行目、メソッド内処理の修正をお願いします。不明な点は質問してください！
-	public List<Subject> get(String subjectCd, String schoolCd) throws Exception{
+	public Subject get(String cd, School school) throws Exception{
 
 		//DB接続
 		Connection con = getConnection();
@@ -32,8 +31,8 @@ public class SubjectDao extends Dao{
 
 		//上記SQL文の?部分を格納
 		//setString, setIntなどデータ型によって使い分けてください(実はsetBooleanとかもある)
-		st.setString(1, schoolCd);
-		st.setString(2, "%"+subjectCd+"%");
+		st.setString(1, school.getCd());
+		st.setString(2, "%"+cd+"%");
 
 		//SQLを実行し、ResultSet型の変数rsに実行結果を格納
 		ResultSet rs = st.executeQuery();
@@ -41,12 +40,13 @@ public class SubjectDao extends Dao{
 		//rsを1行ずつ参照します
 		//検索がきちんとできていれば結果は1行だけですが、
 		//なんかwhile(rs.next())で格納しないとエラーが出ます
+		Subject sub = new Subject();
 		while (rs.next()){
-			Subject sub = new Subject();
-			sub.setSchoolCd(rs.getString("school_cd"));
+			School sc = new School();
+			sc.setCd(rs.getString("school_cd"));
+			sub.setSchool(school);
 			sub.setCd(rs.getString("cd"));
 			sub.setName(rs.getString("name"));
-			subList.add(sub);
 		}
 
 		//このclose()は必ずしてください
@@ -56,7 +56,7 @@ public class SubjectDao extends Dao{
 
 		//return時はデータ型に注意
 		//仕様書の返り値の型はListではなく  Subject  です
-		return Subject;
+		return sub;
 	}
 
 	//科目追加
@@ -64,7 +64,7 @@ public class SubjectDao extends Dao{
 
 	//クラス図情報：
 	//	save(subject:Subject): boolean
-	public int insertSubject(String subjectCd, String subjectName, String schoolCd) throws Exception{
+	public boolean save(Subject subject) throws Exception{
 
 		//DB接続
 		Connection con = getConnection();
@@ -72,9 +72,9 @@ public class SubjectDao extends Dao{
 		//SQL文をセット
 		PreparedStatement st = con.prepareStatement(
 			"insert into subject values(?, ?, ?)");
-		st.setString(1, schoolCd);
-		st.setString(2, subjectCd);
-		st.setString(3, subjectName);
+		st.setString(1, subject.getSchool().getCd());
+		st.setString(2, subject.getCd());
+		st.setString(3, subject.getName());
 
 		//SQLによってDBの更新が行われる場合はexecuteUpdateを使用します(insert,update,deleteのこと)
 		//executeUpdateによって得られる値はint型、更新された行数が入ります
@@ -85,7 +85,12 @@ public class SubjectDao extends Dao{
 		st.close();
 		con.close();
 
-
+		if (line > 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
 		//返り値はbooleanです
 		//return lineでは仕様書に反しますので、
 		//返り値がbooleanになるように処理を追加してください
@@ -94,20 +99,17 @@ public class SubjectDao extends Dao{
 
 		//ClassNumDaoなどで同様の処理をしています。必要であれば参考にしてください！
 
-		return line;
-	}
-
 	//科目情報更新
 	//4/25小柿：使用想定がなさそうです　これはスルーで良いかも
-	public int updateSubject(String schoolCd, String subjectCd, String subjectName) throws Exception{
+	public int updateSubject(School school, String cd, String name) throws Exception{
 		Connection con = getConnection();
 
 		PreparedStatement st = con.prepareStatement(
 			"update subject set name = ? "+
 			"where school_cd = ? and cd = ?");
-		st.setString(1, subjectName);
-		st.setString(2, schoolCd);
-		st.setString(3, subjectCd);
+		st.setString(1, name);
+		st.setString(2, school.getCd());
+		st.setString(3, cd);
 		int line = st.executeUpdate();
 
 		st.close();
@@ -119,7 +121,7 @@ public class SubjectDao extends Dao{
 	//科目削除
 	//クラス図情報：
 	//	delete(subject:Subject): boolean
-	public int deleteSubject(String schoolCd, String subjectCd) throws Exception{
+	public int deleteSubject(School school, String cd) throws Exception{
 
 		//DB接続
 		Connection con = getConnection();
@@ -129,8 +131,8 @@ public class SubjectDao extends Dao{
 			"delete from subject "+
 			"where school_cd = ? "+
 			"and cd = ?");
-		st.setString(1, schoolCd);
-		st.setString(2, subjectCd);
+		st.setString(1, school.getCd());
+		st.setString(2, cd);
 
 		//SQL実行、削除が実行された行数が変数lineに格納されます
 		int line = st.executeUpdate();
