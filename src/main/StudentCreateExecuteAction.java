c1cf6241244f198b4c4d0a79af84b2073d8d03c5
ogata <kmm2459319@stu.o-hara.ac.jp>
2025-05-12 10:52:56 +0900

    確認お願いします。 ※訂正報告のコメントは削除しても大丈夫です。

--- src/main/StudentCreateExecuteAction.java
+++ src/main/StudentCreateExecuteAction.java
@@ -22,86 +22,90 @@ public class StudentCreateExecuteAction extends Action{
 			return "null";
 		}
 
-			///学生登録完了画面の処理
-			//OK。
+		///学生登録完了画面の処理
+		//OK。
 
-			//値の格納
+		//値の格納
 
-			//入学年度
-			String entYearStr = request.getParameter("entYear");
+		//入学年度
+		String entYearStr = request.getParameter("entYear");
 
-			//学生番号
-			String no = request.getParameter("no");
+		//学生番号
+		String no = request.getParameter("no");
 
-			//氏名
-			String name = request.getParameter("name");
+		//氏名
+		String name = request.getParameter("name");
 
-			//クラス
-			String classNum = request.getParameter("classNum");
-
-
-			//登録処理
-			Student s = new Student();
-			s.setNo(no);
-			s.setName(name);
-			s.setEntYear(Integer.parseInt(entYearStr));
-			s.setClassNum(classNum);
-			s.setSchool(user.getSchool());
-
-			StudentDao sDao = new StudentDao(); //学生Dao
-
-			//入力値をチェック エラーメッセージを表示
-
-			//入力年度が選択されてない場合
-			if(entYearStr.equals("none")){
-
-				//シーケンス図を確認してもらえると良いのですが、
-				//セットする値は入学年度ではありません。
-				//そもそも、このif文に分岐するとなるとentYearStrの中身は「none」、
-				//つまり中身がありません。
-				//中身が無いものをセットしても使うことはないかと……
-				request.setAttribute("entYear", entYearStr);
-
-				//messageはOKです！setAttributeはよく使うので引数の形を頭に叩き込んでください。
-				request.setAttribute("message","入学年度を選択してください");
-
-				//シーケンス図情報
-				//ーーーーーーーーーーーーーーーーーーーーーーーーーー
-				//学生番号、氏名、クラス番号をセットし、
-				//「入学年度を選択してください」とメッセージを表示する
-				//ーーーーーーーーーーーーーーーーーーーーーーーーーー
+		//クラス
+		String classNum = request.getParameter("classNum");
 
 
-				//フォワード先はstudent_create_done.jspではないです。
-				//シーケンス図の「alt」と黒枠で囲ってある部分が該当の箇所ですので、
-				//矢印の先にあるファイル名を確認してください。
-				//学生番号が重複している場合のフォワード先も同様です
-				return "student_create_done.jsp";
-			}
+		//登録処理
+		Student s = new Student();
+		s.setNo(no);
+		s.setName(name);
+		s.setEntYear(Integer.parseInt(entYearStr));
+		s.setClassNum(classNum);
+		s.setSchool(user.getSchool());
 
-			//入力された学生番号が重複してる場合
-			if(Objects.nonNull(sDao.get(no))){
-				//ここは入学年度もセットしてください
-				//ここに遷移するなら必ず入学年度は正しく入力されています
-				request.setAttribute("no",no);
-				request.setAttribute("name", name);
-				request.setAttribute("classNum", classNum);
-				request.setAttribute("message", "学生番号が重複しています");
+		StudentDao sDao = new StudentDao(); //学生Dao
 
-				//75行目からのコメントと同じです、こちらもフォワード先を修正してください
-				return "student_create_done.jsp";
+		//入力値をチェック エラーメッセージを表示
 
-			}
+		//入力年度が選択されてない場合
+		if(entYearStr.equals("none")){
 
-			boolean result = sDao.save(s);//学生登録
+			//訂正
+			request.setAttribute("no",no);
+			request.setAttribute("name", name);
+			request.setAttribute("classNum", classNum);
 
-			//変数resultを有効活用してあげるとなお良しです。
-			//具体的には、「DB更新に失敗した場合はエラーページに遷移する」です。
-			//シーケンス図には無いのでチャレンジ課題的な扱いで。
+			//messageはOKです！setAttributeはよく使うので引数の形を頭に叩き込んでください。
+			request.setAttribute("message","入学年度を選択してください");
 
-		// 学生登録完了画面を表示
+			//シーケンス図情報
+			//ーーーーーーーーーーーーーーーーーーーーーーーーーー
+			//学生番号、氏名、クラス番号をセットし、
+			//「入学年度を選択してください」とメッセージを表示する
+			//ーーーーーーーーーーーーーーーーーーーーーーーーーー
 
-		return "student_create_done.jsp";
+			//修正完了
+			//フォワード先はstudent_create.jsp
+			//シーケンス図の「alt」と黒枠で囲ってある部分が該当の箇所ですので、
+			//矢印の先にあるファイル名を確認してください。
+			return "student_create.jsp";
+		}
+
+		//入力された学生番号が重複してる場合
+		if(Objects.nonNull(sDao.get(no))){
+
+			//ここは入学年度もセットしてください
+			//ここに遷移するなら必ず入学年度は正しく入力されています
+
+			//訂正
+			request.setAttribute("entYear", entYearStr);
+			request.setAttribute("no",no);
+			request.setAttribute("name", name);
+			request.setAttribute("classNum", classNum);
+			request.setAttribute("message", "学生番号が重複しています");
+
+			//修正
+			return "student_create.jsp";
+		}
+
+		boolean result = sDao.save(s);//学生登録
+
+		//変数resultを有効活用してあげるとなお良しです。
+		//具体的には、「DB更新に失敗した場合はエラーページに遷移する」です。
+		//シーケンス図には無いのでチャレンジ課題的な扱いで。
+
+		if(result){
+			// 学生登録完了画面を表示
+			return "student_create_done.jsp";
+		}else{
+			// 学生登録完了画面を表示
+			return "student_create_done.jsp";
+		}
 
 		//コードに影響は無いので話半分で良いですが、
 		//途中なんかインデントがズレています。(インデント1つ分右にずれている)
