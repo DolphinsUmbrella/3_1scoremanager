f7fc22bd64840dc8c69863d45d7328caaa2ae173
imkota <imkota@10.251.241.119>
2025-05-20 11:39:44 +0900

    ログイン画面の修正

--- WebContent/login.jsp
+++ WebContent/login.jsp
@@ -15,7 +15,7 @@
       </c:if>
     </div>
 
-    <p><input type = "text" name = "teacher_id" placeholder="半角でご入力ください" value="${teacher_id}" required="required">
+    <p><input type = "text" name = "teacher_id" placeholder="半角でご入力ください" value="${teacher_id}" required="required"><br>
 
     <input type = "password" name = "password" placeholder="30文字以内の半角英数字でご入力ください" required="required"></p>
     <p><input type = "submit" value = "ログイン"></p>
--- src/LoginAction.java
+++ src/LoginAction.java
@@ -1,42 +1,14 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import javax.naming.InitialContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 
 import tool.Action;
 
-public class LoginAction extends Action {
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        Connection con = null;
+public class LoginAction extends Action{
+	public String execute(HttpServletRequest request, HttpServletResponse response
+)throws Exception{
 
-        try {
-            // JNDI (Java Naming and Directory Interface) を使用してデータソースを取得
-            InitialContext ic = new InitialContext();
-            DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/scoremanager"); // context.xml の name 属性と一致させる
-            con = ds.getConnection();
+	return "login.jsp";
 
-            return "login.jsp"; // 正常に処理が完了した場合
+	}
 
-        } catch (SQLException e) {
-            // データベース接続エラー
-            e.printStackTrace();
-
-            request.setAttribute("errorMessage", "データベースへの接続に失敗しました。");
-            request.setAttribute("errorDetail", "SQLException: " + e.getMessage());
-
-            return "error.jsp";
-
-        } finally {
-            if (con != null) {
-                try {
-                    con.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 }
